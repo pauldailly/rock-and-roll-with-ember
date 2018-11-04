7beb03d7ea183439c93cabf5de9ebef5be34b3b7
Paul Dailly <paul.dailly@phorest.com>
2018-11-04 13:17:09 +0000

    - Added custom validator

--- app/controllers/bands/band/details.js
+++ app/controllers/bands/band/details.js
@@ -1,15 +1,26 @@
 import Controller from '@ember/controller';
+import { getWithDefault } from '@ember/object';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
   isEditing: false,
+
+  showErrors: computed('_showErrors', function () {
+    return getWithDefault(this, '_showErrors', { description: false });
+  }),
+
   actions: {
     edit() {
       this.set('isEditing', true);
     },
+    
     async save() {
       let band = this.get('model');
-      await band.save()
-      this.set('isEditing', false);
+      this.set('showErrors.description', true);
+      if (band.get('validations.isValid')) {
+        await band.save();
+        this.set('isEditing', false);
+      }
     }
   }
 });
--- app/routes/bands/band/details.js
+++ app/routes/bands/band/details.js
@@ -1,13 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  
+
   resetController(controller) {
     controller.setProperties({
       isEditing: false,
+      _showErrors: { description: false }
     });
   },
-  
+
   actions: {
     willTransition(transition) {
       if (this.get('controller.isEditing')) {
